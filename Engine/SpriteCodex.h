0ac5c8e52c50de1604b9bd38e4272229d7472b9b
GamingRex4274 <grex4274@gmail.com>
2020-01-16 15:22:32 -0400

    Added SpriteCodex with text drawing functions.

---
+++ Engine/SpriteCodex.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Graphics.h"
+#include "Vei2.h"
+
+class SpriteCodex
+{
+public:
+	static void DrawWaitText(const Vei2& topLeft, Graphics& gfx);
+	static void DrawPlayRptTxt(const Vei2& topLeft, Graphics& gfx);
+public:
+	static constexpr int waitTxtWidth = 378;
+	static constexpr int playTxtWidth = 160;
+	static constexpr int txtHeight = 60;
+};
