0f6452a0e8ede5a79c76a3b4ff13c123557c7de5
GamingRex4274 <grex4274@gmail.com>
2020-01-16 19:37:35 -0400

    Added "game over" text drawing function.

--- Engine/SpriteCodex.h
+++ Engine/SpriteCodex.h
@@ -8,6 +8,7 @@ class SpriteCodex
 public:
 	static void DrawWaitText(const Vei2& topLeft, Graphics& gfx);
 	static void DrawPlayRptTxt(const Vei2& topLeft, Graphics& gfx);
+	static void DrawGameOver(const Vei2& topLeft, Graphics& gfx);
 public:
 	static constexpr int waitTxtWidth = 378;
 	static constexpr int playTxtWidth = 160;
